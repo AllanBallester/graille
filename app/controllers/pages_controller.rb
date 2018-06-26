048bb88c59e8620c1746dd43c837d3e6c9fed972
AllanBallester <allan.ballester@decathlon.com>
2018-06-26 20:20:05 +0200

    homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,17 @@
 class PagesController < ApplicationController
 
   def home
-      @feedbacks = [{name: "Camille",
-  description: " I was intrigued by what was behind Sheepy. Then I made the BAA test and question myself on many aspect on my consumption, way to work, to get impact. Now I can not wait their monthly newsletter to grab new ideas how to reduce and to live rather existing." ,
-  location: "44 BAA  ",
-  picture: "http://res.cloudinary.com/dk9kkvy1o/image/upload/v1522862564/0_2_ksx3g0.jpg"},
-  {name: "Alexandre" ,
-  description: "Minimalist, I was looking to connect with people that shared the same mindset and can pushed me further. I developped how to reduce not only things but sustainable impact, time wasted, social limitations. I left property since 3 months now and improving a lot. ",
-  location: "32 BAA",
-  picture: "http://res.cloudinary.com/dk9kkvy1o/image/upload/v1522862629/0_1_h3l3ua.jpg"},
-  { name: "Elise",
-  description: "My BAA is too bad. But I take as it is, in a funny way. I joined a bikepacking exploration with Sheepy and relearn the pleasure of the real life. I believe that our young generation is unlearning so fast that we need a concrete solution. To leave the mass and become ourself. " ,
-  location: " 86 BAA",
-  picture: "http://res.cloudinary.com/dk9kkvy1o/image/upload/v1522862595/0_3_rbtxh6.jpg"}]
+      @feedbacks = [{name: "Florian",
+  description: " I was worried about my approximative English level and my constraining work hours but finally, I lived this experience as an exchange an not just welcoming someone. Even more, it was a travel with my guest in his own culture." ,
+  location: "SAUVEGARDE DU NORD - PARIS ",
+  picture: "http://res.cloudinary.com/dk9kkvy1o/image/upload/v1530036611/florian_uin9ue.jpg"},
+  {name: "Rachel" ,
+  description: "Alex send me a request and curiously was coming for the same conference like me in Lille. We took this opportunity to share our ideas, networks and even start to co-create together . We could connect more and become close friends !",
+  location: "LEROY MERLIN - LILLE",
+  picture: "http://res.cloudinary.com/dk9kkvy1o/image/upload/v1530036549/rachel_ee6nix.jpg"},
+  { name: "Vishal",
+  description: "I felt like a family member from the moment I entered the house thanks to the way I’ve been welcomed. He allowed me to be myself at his house and I could discover his culture, his city .. things I would never have experienced staying in hotels." ,
+  location: "DECATHLON - BENGALURU ",
+  picture: "http://res.cloudinary.com/dk9kkvy1o/image/upload/v1530036503/vishal_bidcpa.jpg"}]
   end
 end
