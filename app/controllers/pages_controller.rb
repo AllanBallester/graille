f95125b50727479c4faca8aeb9026305d3cd348c
AllanBallester <allan.ballester@decathlon.com>
2018-10-26 22:25:43 +0200

    french version

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,15 @@ class PagesController < ApplicationController
 
   def home
       @feedbacks = [{name: "Maxime",
-  description: "Yes and be careful because I spread the training to all my team today. Even if you do not get any entreprenarial idea in mind, this training allow I think to acquire a methodology for all creative projects" ,
+  description: "Wow, yes ! J'ai passé une excellente journée, et une mauvaise nuit... Ca remue pas mal, j'attendais du concret et me voilà aujourd'hui auto-entrepreneur. La méthode est imparable, et permet réellement de se lancer dans l'aventure. Ta personnalité aussi, franchement c'est inspirant." ,
   location: "USER LEADER - DECATHLON ",
   picture: "https://res.cloudinary.com/dk9kkvy1o/image/upload/v1540578658/0_7_hxr89f.jpg"},
   {name: "DELPHINE" ,
-  description: "I loved the training ! It was concrete, dynamic and clear. It allow to understand easily the concept of Lean Startup and to put it into practice with your own entrepreneurship.Even it accelerated it",
+  description: "J'ai adoré Lean Startup 1. Elle est concrète, rythmée, dynamique et claire. Elle permet de bien comprendre le concept Lean Start up et de mettre en pratique avec son propre projet. Celà m'a même permis d'accélérer.",
   location: "INTRAPRENEUR- DECATHLON ",
   picture: "https://res.cloudinary.com/dk9kkvy1o/image/upload/v1540578609/0_6_ox1qz5.jpg"},
   { name: "RAVI",
-  description: "It's compulsory nowadays to implement the Lean Startup into our management I had the chance to live it with some of our Store Leaders and I can testify that it will give them a step ahead from other retailers." ,
+  description: "C'est indispensable aujourd'hui d'implémenter une culture Lean Startup au sein de nos équipes dont leur business sont en mouvance constante. J'ai eu la chance de vivre la formation avec mes équipes retail. Je peux dire qu'ils prennent une longueur d'avance." ,
   location: "HR LEADER - DECATHLON INDIA",
   picture: "https://res.cloudinary.com/dk9kkvy1o/image/upload/v1540578995/0_8_zkdwfj.jpg"}]
   end
